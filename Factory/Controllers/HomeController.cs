9da56f8322916660642f6b0fef6e0f186c3bd48a
Molly Donegan <mollyrdonegan@gmail.com>
2023-03-17 10:49:17 -0700

    add controllers

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Factory.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
+    }
+}
